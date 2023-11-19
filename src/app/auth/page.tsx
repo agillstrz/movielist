fb773bdd6685d757107b71b6e876646d37f8d201
agillstrz <agillstrz@gmail.com>
2023-11-20 02:25:10 +0700

    update route

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -10,12 +10,7 @@ export default async function Auth() {
     redirect("/");
   }
   return (
-    <div
-      className="flex w-screen h-screen   bg-base fixed"
-      // style={{
-      //   backgroundImage: `url('https://images.unsplash.com/photo-1488554378835-f7acf46e6c98?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80')`,
-      // }}
-    >
+    <div className="flex w-screen h-screen   bg-base fixed">
       <div className="w-full  flex justify-center items-center fixed backdrop-blur-sm bg-cover bg-center h-full">
         <CardLogin />
       </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,7 +13,6 @@ export default function Button({
   label,
   icon,
 }: ButtonProps) {
-  const { push } = useRouter();
   return (
     <button
       onClick={onClick}
